f26b5ab273ae1c7ab8f68b9e20b208df18663371
Jose Fuilco <josefuilco@gmail.com>
2024-12-07 16:59:29 -0500

    Fix: Agregando controladores al modulo de authentication

--- server/src/authentication/auth.module.ts
+++ server/src/authentication/auth.module.ts
@@ -1,11 +1,13 @@
 import { Logger, Module } from "@nestjs/common";
 import { UserController } from "./infrastructure/web/controller/user.controller";
+import { AuthController } from "./infrastructure/web/controller/auth.controller";
+import { TokenController } from "./infrastructure/web/controller/token.controller";
 import { USER_REPOSITORY } from "./domain/repository/user.repository";
 import UserService from "./application/service/user.service";
 import MysqlUserRepository from "./infrastructure/repository/mysql-user.repository";
 
 @Module({
-  controllers: [UserController],
+  controllers: [UserController, AuthController, TokenController],
   providers: [
     UserService,
     {
